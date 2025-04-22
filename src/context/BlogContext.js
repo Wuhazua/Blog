53b710180cd8b61cdd725a50da8490ead3c3bf0e
Wuhazu <wuhazu18@gmail.com>
2025-04-22 20:45:12 +0200

    Update BlogContext.js

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,1 +1,19 @@
+import React, { createContext, useContext, useState } from 'react';
 
+const BlogContext = createContext();
+
+export const useBlogContext = () => useContext(BlogContext);
+
+export const BlogProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
+
+  return (
+    <BlogContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </BlogContext.Provider>
+  );
+};
