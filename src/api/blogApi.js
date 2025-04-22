094556c2c4686c77af5a853c23d436f6790345f8
Wuhazu <wuhazu18@gmail.com>
2025-04-22 20:44:55 +0200

    Update blogApi.js

--- src/api/blogApi.js
+++ src/api/blogApi.js
@@ -1,1 +1,22 @@
-
+export const fetchBlogPosts = async () => {
+  
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
+        {
+          id: 1,
+          title: 'How to Build a React Portfolio',
+          content: 'Content goes here...',
+          date: '2025-04-01',
+        },
+        {
+          id: 2,
+          title: 'Understanding JavaScript Closures',
+          content: 'Content goes here...',
+          date: '2025-03-15',
+        },
+        
+      ]);
+    }, 1500); 
+  });
+};
