b6c7a803af1e984ed1d7c65c55c54bf0b7f4e8e9
Wuhazu <wuhazu18@gmail.com>
2025-04-22 20:45:21 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,19 @@
+import React from 'react';
+import Header from './components/Header';
+import BlogPostList from './components/BlogPostList';
+import Footer from './components/Footer';
+import { BlogProvider } from './context/BlogContext';
 
+const App = () => {
+  return (
+    <BlogProvider>
+      <div className="app">
+        <Header />
+        <BlogPostList />
+        <Footer />
+      </div>
+    </BlogProvider>
+  );
+};
+
+export default App;
